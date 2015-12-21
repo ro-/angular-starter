813adcadec264e2ee365722e7bc20400fbb927d4
ro- <ratboystylez@gmail.com>
2015-12-21 21:18:00 +1100

    jsdoc

--- gruntfile.js
+++ gruntfile.js
@@ -1,8 +1,15 @@
 module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-jsdoc');
 
   grunt.initConfig({
+    
+    'pkg': grunt.file.readJSON('package.json'),
+    
     'meta': {
       'jsFilesForTesting': [
         'bower_components/jquery/jquery.js',
@@ -26,10 +33,66 @@ module.exports = function (grunt) {
             'source/**/*.js'
           ],
         }
+      },
+      'dist': {
+        'options': {
+          'configFile': 'karma.conf.js',
+          'files': [
+            '<%= meta.jsFilesForTesting %>',
+            'dist/<%= pkg.namelower %>-<%= pkg.version %>.js'
+          ],
+        }
+      },
+      'minified': {
+        'options': {
+          'configFile': 'karma.conf.js',
+          'files': [
+            '<%= meta.jsFilesForTesting %>',
+            'dist/<%= pkg.namelower %>-<%= pkg.version %>.min.js'
+          ],
+        }
       }
-	}
+	},
+  
+  'jshint': {
+      'beforeconcat': ['source/**/*.js'],
+    },
+    
+    'concat': {
+      'dist': {
+        'src': ['source/**/*.js'],
+        'dest': 'dist/<%= pkg.namelower %>-<%= pkg.version %>.js'
+      }
+    },
+
+    'uglify': {
+      'options': {
+        'mangle': false
+      },  
+      'dist': {
+        'files': {
+          'dist/<%= pkg.namelower %>-<%= pkg.version %>.min.js': ['dist/<%= pkg.namelower %>-<%= pkg.version %>.js']
+        }
+      }
+    },
+    
+    'jsdoc': {
+      'src': ['source/**/*.js'],
+      'options': {
+        'destination': 'doc'
+      }
+    }
   });
 
   grunt.registerTask('test', ['karma:development']);
-
+grunt.registerTask('build',
+    [
+      'jshint',
+      'karma:development',
+      'concat',
+      'karma:dist',
+      'uglify',
+      'karma:minified',
+      'jsdoc'
+    ]);
 };
